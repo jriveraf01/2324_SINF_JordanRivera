5bf67fcb68e827850dde63d2ab6a4d2c23375e3a
jriveraf01 <jriveraf01@educarex.es>
2024-02-27 18:22:17 +0100

    update

--- Repaso2/ej1.sh
+++ Repaso2/ej1.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-$fichero=productos
-
-##$fichero=`grep -r "1."| cut -d f1`
-
-producto=`grep -r "1." productos`
-
+FICHERO=`cat productos | grep -v "^#"`
 IFS=$'\n'
+contador=1
+for linea in $FICHERO; do 
+    echo"$contador.$linea" >> conNumero
+    contador=$(($contador + 1))
+done
+cat conNumero | cut -d';' -f1
 
-for linea
+read -p "Elige el producto para mostar su precio" numeroProducto
+productoAbuscar=`grep "^numeroProducto." conNumero | cut -d';' -f1| cut -d'.' -f2`
+precio=`grep "^$numeroProducto." conNumero | cut -d';' -f2`
+echo "El precio del producto $productoAbuscar es $precio"
+
+rm conNumero
